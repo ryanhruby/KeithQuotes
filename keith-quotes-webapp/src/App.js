e7c04dd96d83c22356837d611cd800899cfd05b6
Vivian Jacobitz <jacobitz.vivian@cox.net>
2022-03-22 18:21:49 -0500

    Updated images and start of API work

--- keith-quotes-webapp/src/App.js
+++ keith-quotes-webapp/src/App.js
@@ -1,67 +1,84 @@
 import "./App.css";
 
+function sendMessage(message) {
+  // var twilio = require("twilio");
+
+  // var client = new twilio("ACf7aabced2a6fd9e9c72f2b57d8127981", "f42ada5e2861d45d77dd874ddd2f6554");
+
+  // client.messages
+  // .create({
+  //   to: '+14027696011',
+  //   from: '+12087470934',
+  //   body: message
+  // })
+  // .catch(error => console.error(error));
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="keith-logo.png" className="App-logo" alt="keith-logo" />
- 
+      <header className="App-header"> 
+        <strong>Life Advice with Keith</strong>
       </header>
       <body>
-      <div id="quote_button_1">
-          <button>
-            <img
-              src="Keith1.png"
-              width="200"
-              height="200"
-              alt="keith-1"
-              onClick={console.log("CLICKED")}
-            />
-          </button>
-        </div>
-        <div id="quote_button_2">
-          <button>
-            <img
-              src="Keith2.png"
-              width="200"
-              height="200"
-              alt="keith-2"
-              onClick={console.log("CLICKED")}
-            />
-          </button>
-        </div>
-        <div id="quote_button_3">
-          <button>
-            <img
-              src="Keith3.png"
-              width="200"
-              height="200"
-              alt="keith-3"
-              onClick={console.log("CLICKED")}
-            />
-          </button>
-        </div>
-        <div id="quote_button_4">
-          <button>
-            <img
-              src="Keith4.png"
-              width="200"
-              height="200"
-              alt="keith-4"
-              onClick={console.log("CLICKED")}
-            />
-          </button>
-        </div>
-        <div id="quote_button_5">
-          <button>
-            <img
-              src="Keith5.png"
-              width="200"
-              height="200"
-              alt="keith-5"
-              onClick={console.log("CLICKED")}
-            />
-          </button>
+        <img src="keith-logo.png" className="App-logo" alt="keith-logo" />
+
+        <div id="button-row" class="button-row">
+          <div id="quote_button_1" class="button-div">
+            <button>
+              <img
+                src="keith-1.png"
+                width="200"
+                height="200"
+                alt="keith-1"
+                onClick={sendMessage("test")}
+              />
+            </button>
+          </div>
+          <div id="quote_button_2" class="button-div">
+            <button>
+              <img
+                src="keith-2.png"
+                width="200"
+                height="200"
+                alt="keith-2"
+                onClick={console.log("CLICKED")}
+              />
+            </button>
+          </div>
+          <div id="quote_button_3" class="button-div">
+            <button>
+              <img
+                src="keith-3.png"
+                width="200"
+                height="200"
+                alt="keith-3"
+                onClick={console.log("CLICKED")}
+              />
+            </button>
+          </div>
+          <div id="quote_button_4" class="button-div">
+            <button>
+              <img
+                src="keith-4.png"
+                width="200"
+                height="200"
+                alt="keith-4"
+                onClick={console.log("CLICKED")}
+              />
+            </button>
+          </div>
+          <div id="quote_button_5" class="button-div">
+            <button>
+              <img
+                src="keith-5.png"
+                width="200"
+                height="200"
+                alt="keith-5"
+                onClick={console.log("CLICKED")}
+              />
+            </button>
+          </div>
         </div>
       </body>
     </div>
