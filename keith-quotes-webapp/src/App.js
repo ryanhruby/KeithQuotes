f7f467416f330ba611f1be76647fca126bdde145
ASeibel2 <54328554+ASeibel2@users.noreply.github.com>
2022-03-22 17:38:06 -0500

    Added 5 button

--- keith-quotes-webapp/src/App.js
+++ keith-quotes-webapp/src/App.js
@@ -5,16 +5,65 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="keith-logo.png" className="App-logo" alt="keith-logo" />
-        <div>
+ 
+      </header>
+      <body>
+      <div id="quote_button_1">
           <button>
             <img
-              src="Keith-husker.png"
+              src="Keith1.png"
+              width="200"
+              height="200"
               alt="keith-1"
               onClick={console.log("CLICKED")}
             />
           </button>
         </div>
-      </header>
+        <div id="quote_button_2">
+          <button>
+            <img
+              src="Keith2.png"
+              width="200"
+              height="200"
+              alt="keith-2"
+              onClick={console.log("CLICKED")}
+            />
+          </button>
+        </div>
+        <div id="quote_button_3">
+          <button>
+            <img
+              src="Keith3.png"
+              width="200"
+              height="200"
+              alt="keith-3"
+              onClick={console.log("CLICKED")}
+            />
+          </button>
+        </div>
+        <div id="quote_button_4">
+          <button>
+            <img
+              src="Keith4.png"
+              width="200"
+              height="200"
+              alt="keith-4"
+              onClick={console.log("CLICKED")}
+            />
+          </button>
+        </div>
+        <div id="quote_button_5">
+          <button>
+            <img
+              src="Keith5.png"
+              width="200"
+              height="200"
+              alt="keith-5"
+              onClick={console.log("CLICKED")}
+            />
+          </button>
+        </div>
+      </body>
     </div>
   );
 }
