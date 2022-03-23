a75df248c944fb663320d63963e90ccd33748e5d
ryanhruby <ryanhruby549@gmail.com>
2022-03-22 21:03:52 -0500

    Server side commit

--- keith-quotes-server/environment.js
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    account_sid: process.env.REACT_APP_TWILIO_ACCOUNT_SID,
-    auth_token: process.env.REACT_APP_TWILIO_AUTH_TOKEN,
-    number: process.env.REACT_APP_TWILIO_NUMBER
-}
