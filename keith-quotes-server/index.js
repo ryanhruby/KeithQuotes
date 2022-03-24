fcee2324a64a9ea20d9f56523a9637feaa7f9dc2
Vivian Jacobitz <jacobitz.vivian@cox.net>
2022-03-23 20:26:19 -0500

    Finished code

--- keith-quotes-server/index.js
+++ keith-quotes-server/index.js
@@ -17,14 +17,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(pino);
 
+var numbers = [
+  "+14026814485",
+  "+14027696011",
+  "+14028007618",
+  "+14026998121",
+  "+14023047125",
+  "+19139075948",
+  "+14028020212",
+  "+16605410202",
+  "+16128026214",
+  "+18126050116",
+  "+14023805165",
+  "+14022011809",
+  "+14022094668",
+  "+14026010106",
+  "+14023144546",
+];
+
 app.post("/send/message1", (req, res) => {
   res.header("Content-Type", "application/json");
-  client.messages
+  numbers.map(number => { 
+    return client.messages
     .create({
       username: "jacobitz.vivian@cox.net",
-      body: "Keith Quote 1",
+      body: "Keith's first quote: \"I didn't think there was water in Nebraska City.\"",
       from: process.env.TWILIO_NUMBER,
-      to: "+14026814485",
+      to: number,
     })
     .then(() => {
       res.send(JSON.stringify({ success: true }));
@@ -33,16 +52,18 @@ app.post("/send/message1", (req, res) => {
       console.log(err);
       res.send(JSON.stringify({ success: false }));
     });
+  });
 });
 
 app.post("/send/message2", (req, res) => {
   res.header("Content-Type", "application/json");
-  client.messages
+  numbers.map(number => { 
+    return client.messages
     .create({
       username: "jacobitz.vivian@cox.net",
-      body: "Keith Quote 1",
+      body: "Keith's second quote: \"I reject the concept of empathy.\"",
       from: process.env.TWILIO_NUMBER,
-      to: "+14026814485",
+      to: number,
     })
     .then(() => {
       res.send(JSON.stringify({ success: true }));
@@ -51,16 +72,18 @@ app.post("/send/message2", (req, res) => {
       console.log(err);
       res.send(JSON.stringify({ success: false }));
     });
+  });
 });
 
 app.post("/send/message3", (req, res) => {
   res.header("Content-Type", "application/json");
-  client.messages
+  numbers.map(number => { 
+    return client.messages
     .create({
       username: "jacobitz.vivian@cox.net",
-      body: "Keith Quote 1",
+      body: "Keith's third quote: \"Work your way up? Work hard? That's bullshit!\"",
       from: process.env.TWILIO_NUMBER,
-      to: "+14026814485",
+      to: number,
     })
     .then(() => {
       res.send(JSON.stringify({ success: true }));
@@ -69,16 +92,18 @@ app.post("/send/message3", (req, res) => {
       console.log(err);
       res.send(JSON.stringify({ success: false }));
     });
+  });
 });
 
 app.post("/send/message4", (req, res) => {
   res.header("Content-Type", "application/json");
-  client.messages
+  numbers.map(number => { 
+    return client.messages
     .create({
       username: "jacobitz.vivian@cox.net",
-      body: "Keith Quote 1",
+      body: "Keith's fourth quote: \"My morals are flexible.\"",
       from: process.env.TWILIO_NUMBER,
-      to: "+14026814485",
+      to: number,
     })
     .then(() => {
       res.send(JSON.stringify({ success: true }));
@@ -87,16 +112,18 @@ app.post("/send/message4", (req, res) => {
       console.log(err);
       res.send(JSON.stringify({ success: false }));
     });
+  });
 });
 
 app.post("/send/message5", (req, res) => {
   res.header("Content-Type", "application/json");
-  client.messages
+  numbers.map(number => { 
+    return client.messages
     .create({
       username: "jacobitz.vivian@cox.net",
-      body: "Keith Quote 1",
+      body: "Keith's fifth quote: \"If you compete for a single man, you're competing against every other girl... If you compete for a married man, it's only one girl.\"",
       from: process.env.TWILIO_NUMBER,
-      to: "+14026814485",
+      to: number,
     })
     .then(() => {
       res.send(JSON.stringify({ success: true }));
@@ -105,6 +132,7 @@ app.post("/send/message5", (req, res) => {
       console.log(err);
       res.send(JSON.stringify({ success: false }));
     });
+  });
 });
 
 app.listen(port, () => {
